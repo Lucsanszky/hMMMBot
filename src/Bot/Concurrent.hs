df3b22cfe0fa84b04498bf412a61a1fe39ba7b18
Lucsanszky <dan.lucsanszky@gmail.com>
2018-06-20 20:47:44 +0100

    Remove unused waitTilProcessed function

--- src/Bot/Concurrent.hs
+++ src/Bot/Concurrent.hs
@@ -52,30 +52,3 @@ readResponse q = do
     case r of
         Nothing -> retry
         Just x  -> return x
-
-waitTilProcessed :: BotState -> STM Bool
-waitTilProcessed BotState {..} = do
-    O (TABLE {_data = orderData}) <- readResponse orderQueue
-    case orderData !? 0 of
-        Nothing -> retry
-        Just (RespOrder {clOrdID = id}) ->
-            case id of
-                Nothing -> retry
-                Just clientId -> do
-                    Exe (TABLE {_data = executionData}) <-
-                        readResponse executionQueue
-                    case executionData !? 0 of
-                        Nothing -> retry
-                        Just (RespExecution { clOrdID = respId
-                                            , workingIndicator = working
-                                            }) ->
-                            case respId of
-                                Nothing -> retry
-                                Just x ->
-                                    case (fmap
-                                              (&& (x ==
-                                                   clientId))
-                                              working) of
-                                        Just True ->
-                                            return True
-                                        _ -> retry
