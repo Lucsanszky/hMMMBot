14c9741571b1b5075cabe7f96047d6ded12d1036
Lucsanszky <dan.lucsanszky@gmail.com>
2018-06-28 20:49:45 +0100

    Add ingest node pipeline config

--- src/Bot/Logging.hs
+++ src/Bot/Logging.hs
@@ -38,13 +38,13 @@ esLoggingContext :: Text -> Text -> LogContext -> IO LogContext
 esLoggingContext user pw cxt
  = do
     mgr <- newManager tlsManagerSettings
-    let bhe = mkBHEnv (Server "https://61abc218de484ebd8e5b1cb984092716.eu-west-1.aws.found.io:9243") mgr
+    let bhe = mkBHEnv (Server "https://61abc218de484ebd8e5b1cb984092716.eu-west-1.aws.found.io:9243/") mgr
     esScribe <-
         mkEsScribe
             defaultEsScribeCfgV5
             bhe { bhRequestHook = basicAuthHook (EsUsername user) (EsPassword pw)}
-            (IndexName "katip")
-            (MappingName "hMMMBot-logs")
+            (IndexName "trading-session")
+            (MappingName "hMMMBot")
             DebugS
             V3
     registerScribe "es" esScribe defaultScribeSettings cxt
