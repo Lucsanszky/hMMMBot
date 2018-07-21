ad531ca75f4200805ec35bfa1b4f88561993545c
Lucsanszky <dan.lucsanszky@gmail.com>
2018-07-21 15:08:52 +0100

    Add getPrice and getId conversion functions

--- src/Bot/Math.hs
+++ src/Bot/Math.hs
@@ -1,6 +1,8 @@
 module Bot.Math
     ( roundPrice
     , convert
+    , getPrice
+    , getId
     ) where
 
 import           BasicPrelude
@@ -14,3 +16,9 @@ convert XBt_to_XBT = (* (1 / 100000000))
 convert XBT_to_XBt = (*) 100000000
 convert USD_to_XBT = (/) 1
 convert XBT_to_USD = (*) 1
+
+getPrice :: Integer -> Double
+getPrice pricId = ((100000000 * 88) - fromIntegral pricId) * 0.01
+
+getId :: Double -> Integer
+getId price = truncate $ (100000000 * 88) - (price / 0.01)
