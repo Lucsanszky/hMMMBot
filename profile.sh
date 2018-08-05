f66f48564e8e750b0b58d33cd1f6760fee26b4da
Lucsanszky <dan.lucsanszky@gmail.com>
2018-08-05 15:04:21 +0200

    Modify profiling and benchmarking scripts

--- profile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cabal new-run bench-bot -O2 --enable-profiling --profiling-detail=all-functions --enable-executable-profiling --enable-library-profiling -- ~/.ssh/bmextestnet.pub ~/.ssh/bmextestnet ~/.ssh/elastic.id ~/.ssh/elastic.pw +RTS -p -s
